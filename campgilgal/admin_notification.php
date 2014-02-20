64c4fb83b060cb9dbb5ef13333e612bcb9e219ee
mlisondra <milder.lisondra@lisondraconsulting.com>
2014-02-20 15:54:29 -0800

    Text changes, processing

--- campgilgal/admin_notification.php
+++ campgilgal/admin_notification.php
@@ -1,79 +1,76 @@
 <table width="600px">
 <tr>
-<td><h2>Camp Gilgal Registration Notification</h2></td>
+<td style="font-family:Helvetica, Arial, sans-serif;"><h3>A user has registered for Camp Gilgal</h3></td>
 </tr>
 <tr>
-<td><h3>A user has registered for Camp Gilgal</h3></td>
-</tr>
-<tr>
-<td>Please find below the registration information.</td>
+<td style="font-weight:bold;color:red;font-family:Helvetica, Arial, sans-serif;font-size:10pt;">Please find below the registration information.</td>
 </tr>
 <tr>
 <td>
 <!-- Basic Information - Start -->
 <table width="100%">
 <tr>
-<td colspan="3" bgcolor="#79BAEC">&nbsp;&nbsp;<strong>BASIC INFORMATION</strong></td>
+<td colspan="3" bgcolor="#79BAEC" style="font-family:Helvetica, Arial, sans-serif;"><strong>BASIC INFORMATION</strong></td>
 </tr>
 <tr>
-<td align="left" ><strong>User's IP Address</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>User's IP Address</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{IPADDRESS}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{IPADDRESS}</td>
 </tr>
 <tr>
-<td align="left" ><strong>First Name</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>First Name</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{fname}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{fname}</td>
 </tr>
 <tr>
-<td align="left" ><strong>Last Name</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Last Name</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{lname}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{lname}</td>
 </tr>
 <tr>
-<td align="left"><strong>Phone</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Phone</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{phone}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{phone}</td>
 </tr>
 <tr>
-<td align="left" ><strong>Email</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Email</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{email}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{email}</td>
 </tr>
 <tr>
-<td align="left" ><strong>Address</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Address</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{address_1}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{address_1}</td>
 </tr>
 <tr>
-<td align="left" ><strong> &nbsp; </strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong> &nbsp; </strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{address_2}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{address_2}</td>
 </tr>
 <tr>
-<td align="left" ><strong>City</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>City</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{city}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{city}</td>
 </tr>
 <tr>
-<td align="left" ><strong>State/Province</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>State/Province</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{state_province}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{state_province}</td>
 </tr>
 <tr>
-<td align="left" ><strong>Postal/Zip Code</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Postal/Zip Code</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{postal_zip}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{postal_zip}</td>
 </tr>
 <tr>
-<td align="left" ><strong>Country</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Country</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{country}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{country}</td>
 </tr>
 <tr>
-<td align="left" ><strong>I am Jewish</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>I am Jewish</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" ><?php
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><?php
 $heritage = $form->data['chronoform_data']['heritage'];
 if($heritage == 'Jewish'){
 	echo "Yes";
@@ -83,9 +80,9 @@ if($heritage == 'Jewish'){
 ?></td>
 </tr>
 <tr>
-<td align="left" ><strong>I Believe in Jesus</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>I Believe in Jesus</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left"><?php
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><?php
 $belief = $form->data['chronoform_data']['belief'];
 if($belief == 'Believe in Jesus' || $belief == 'Do believe in Jesus'){
 	echo "Yes";
@@ -103,65 +100,51 @@ if($belief == 'Believe in Jesus' || $belief == 'Do believe in Jesus'){
 <!-- Camper Information - Start -->
 <table width="100%">
 <tr>
-<td bgcolor="#79BAEC" colspan="7">&nbsp;&nbsp;<strong>CAMPER INFORMATION</strong></td>
+<td bgcolor="#79BAEC" colspan="7" style="font-family:Helvetica, Arial, sans-serif;"><strong>CAMPER INFORMATION</strong></td>
 </tr>
 <tr>
-<td align="left" colspan="7"><strong>Number of registered child(ren):</strong>&nbsp;&nbsp;&nbsp;{child_count}</td>
+<td align="left" colspan="7" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Number of registered child(ren):</strong>&nbsp;&nbsp;&nbsp;{child_count}</td>
 </tr>
 <tr>
-<td><strong>Child's Name</strong></td>
+<td style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Child's Name</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td><strong>Date of Birth</strong></td>
+<td style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Date of Birth</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td><strong>Gender</strong></td>
+<td style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Gender</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td><strong>Camp</strong></td>
-</tr>
-<tr>
-<td>{child_1_name}</td>
-<td>&nbsp;&nbsp;&nbsp;</td>
-<td>{child_1_dob}</td>
-<td>&nbsp;&nbsp;&nbsp;</td>
-<td>{child_1_gender}</td>
-<td>&nbsp;&nbsp;&nbsp;</td>
-<td>
-
-<?php
-	if(input_checkbox_group1 != ''){
-		print $form->data['chronoform_data']['input_checkbox_group1'];
-	}
-	if(input_checkbox_group2 != ''){
-		print $form->data['chronoform_data']['input_checkbox_group2'];
-	}
-	if(input_checkbox_group3 != ''){
-		print $form->data['chronoform_data']['input_checkbox_group3'];
-	}
-?>	
-</td>
+<td style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Camp</strong></td>
 </tr>
 <?php
 /* check and loop through all children's records */
 $childctr = $form->data['chronoform_data']['child_count'];
-for($x = 2;$x <= $childctr; $x++){ 
+for($x = 1;$x <= $childctr; $x++){ 
 ?>
 <tr>
-<td>{child_<?php echo $x; ?>_name}</td>
+<td style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{child_<?php echo $x; ?>_name}</td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td>{child_<?php echo $x; ?>_dob}</td>
+<td style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{child_<?php echo $x; ?>_dob}</td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td>{child_<?php echo $x; ?>_gender}</td>
+<td style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{child_<?php echo $x; ?>_gender}</td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td>
+<td style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{region_label}: 
 	<?php
 		$key = "child_" . $x . "_camps";
-		print $form->data['chronoform_data'][$key];
+		$camp_label = str_replace("(Midwest)","",$form->data['chronoform_data'][$key]);
+		$camp_label = str_replace("(West)","",$camp_label);
+		$camp_label = str_replace("(East)","",$camp_label);
+		$camp_label = str_replace("WWW Northern California","WWW North",$camp_label);
+		$camp_label = str_replace("WWW Southern California","WWW South",$camp_label);
+		$camp_label = str_replace("Wonderful Winter Weekend","WWW",$camp_label);
+		$camp_label = str_replace(" ,",",",$camp_label);
+		
+		print $camp_label;
 	?>
 </td>
 </tr>
 <?php } ?>
-<!--<tr>
-<td colspan="7" align="center"><span style="font-weight: bold; color: #ff0000;">Total Registration Fees:</span>&nbsp;$ {total_reg_fee}</td>
-</tr>-->
+<tr>
+<td colspan="7" align="center" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><a href="https://store.jewsforjesus.org/index.php/event_payment/?event_type={event_type}&uid={registration_id}&amount={total_reg_fee}" target="_blank">Payment Link</a></td>
+</tr>
 </table>
 <!-- Children Information - End -->
 </td>
@@ -176,50 +159,50 @@ if($friendName)
 <td>
 <table width="100%">
 <tr>
-<td bgcolor="#79BAEC" colspan="3">&nbsp;&nbsp;<strong>SEND US A NAME</strong></td>
+<td bgcolor="#79BAEC" colspan="3" style="font-family:Helvetica, Arial, sans-serif;"><strong>SEND US A NAME</strong></td>
 </tr>
 <tr>
-<td colspan="3"><em>A Jewish family who might want to send their child(ren) to Camp Gilgal.</em></td>
+<td colspan="3" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><em>A Jewish family who might want to send their child(ren) to Camp Gilgal.</em></td>
 </tr>
 <tr>
-<td align="left" ><strong>Family's Name</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Family's Name</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{refer_name}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{refer_name}</td>
 </tr>
 <tr>
-<td align="left" ><strong>Address</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Address</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{refer_address_1}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{refer_address_1}</td>
 </tr>
 <tr>
-<td align="left" ><strong>&nbsp;</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>&nbsp;</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{refer_address_2}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{refer_address_2}</td>
 </tr>
 <tr>
-<td align="left" ><strong>City</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>City</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{refer_city}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{refer_city}</td>
 </tr>
 <tr>
-<td align="left" ><strong>State/Province</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>State/Province</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{refer_state}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{refer_state}</td>
 </tr>
 <tr>
-<td align="left" ><strong>Postal/Zip Code</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Postal/Zip Code</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{refer_zip}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{refer_zip}</td>
 </tr>
 <tr>
-<td align="left" ><strong>Country</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Country</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{refer_country}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{refer_country}</td>
 </tr>
 <tr>
-<td align="left" ><strong>Phone</strong></td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><strong>Phone</strong></td>
 <td>&nbsp;&nbsp;&nbsp;</td>
-<td align="left" >{refer_phone}</td>
+<td align="left" style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{refer_phone}</td>
 </tr>
 </table>
 </td>
@@ -235,14 +218,14 @@ $comments = $form->data['chronoform_data']['comments'];
 if($comments)
 { ?>
 <tr>
-<td bgcolor="#79BAEC">&nbsp;&nbsp;<strong>OTHER INFORMATION</strong></td>
+<td bgcolor="#79BAEC" style="font-family:Helvetica, Arial, sans-serif;">&nbsp;&nbsp;<strong>OTHER INFORMATION</strong></td>
 </tr>
 <tr>
-<td><em>Comments and questions during the registration.</em>
+<td style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><em>Comments and questions during the registration.</em>
 </td>
 </tr>
 <tr>
-<td>{comments}
+<td style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{comments}
 </td>
 </tr>
 <?php
@@ -255,11 +238,11 @@ $howDid = $form->data['chronoform_data']['how_did_you'];
 if($howDid)
 { ?>
 <tr>
-<td><p><strong>How did you hear about us?</strong></p>
+<td style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;"><p><strong>How did you hear about us?</strong></p>
 </td>
 </tr>
 <tr>
-<td>{how_did_you}
+<td style="font-family:Helvetica, Arial, sans-serif;font-size:10pt;">{how_did_you}
 </td>
 </tr>
 <?php
